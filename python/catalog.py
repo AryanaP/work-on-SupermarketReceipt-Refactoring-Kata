bdc216063cac793d7fce5770d1800568c9ea9f94
Aryana Pezé <aryana.peze@octo.com>
2019-10-02 08:41:26 +0200

    initial code

---
+++ python/catalog.py
@@ -0,0 +1,12 @@
+
+class SupermarketCatalog:
+
+    def add_product(self, product, price):
+        raise Exception("cannot be called from a unit test - it accesses the database")
+
+    def unit_price(self, product):
+        raise Exception("cannot be called from a unit test - it accesses the database")
+
+
+
+# Methodes vont chercher dans la BDD mais pas développé
